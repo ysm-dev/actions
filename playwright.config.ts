dd2656a2cc274c09f48369f73449ec5c58190825
Chris <ysm0622@gmail.com>
2023-02-15 19:22:18 +0900

    infinite

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,20 +3,20 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './src',
   /* Maximum time one test can run for. */
-  timeout: 60 * 10000,
+  timeout: Infinity,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000,
+    // timeout: 5000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: Infinity,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
