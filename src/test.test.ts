a318aefa16d6c50915b73473a0254fa3f72ce903
ysm-dev <i@ysm.dev>
2024-02-26 15:13:24 +0900

    FIx

--- src/test.test.ts
+++ src/test.test.ts
@@ -1,14 +1,14 @@
-import { test, expect } from '@playwright/test'
-import StealthPlugin from 'puppeteer-extra-plugin-stealth'
-import { chromium } from 'playwright-extra'
-import fetch from 'isomorphic-fetch'
+import { test, expect } from "@playwright/test";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import { chromium } from "playwright-extra";
+import fetch from "isomorphic-fetch";
 
-test('Test', async ({}) => {
-  let blockCount = 0
-  let browser: any = null
-  let page: any = null
+test("Test", async ({}) => {
+  let blockCount = 0;
+  let browser: any = null;
+  let page: any = null;
 
-  chromium.use(StealthPlugin())
+  chromium.use(StealthPlugin());
 
   while (true) {
     try {
@@ -16,15 +16,15 @@ test('Test', async ({}) => {
         browser = await chromium.launch({
           headless: false,
           devtools: true,
-        })
+        });
       }
 
       if (!page) {
-        page = await browser!.newPage()
-        await page?.goto(`https://www.crunchbase.com/browser-extension`)
+        page = await browser!.newPage();
+        await page?.goto(`https://www.crunchbase.com/browser-extension`);
       }
 
-      console.log('start!')
+      console.log("start!");
 
       // await page?.goto('https://bot.sannysoft.com/')
 
@@ -32,114 +32,114 @@ test('Test', async ({}) => {
         page?.evaluate(() => {
           const isBlocked = () =>
             !!document.querySelector(
-              'body > section > div.page-title-wrapper > div > h1',
-            )
-          return isBlocked()
-        })
+              "body > section > div.page-title-wrapper > div > h1"
+            );
+          return isBlocked();
+        });
 
       // if blocked twice, close browser
       if (await isBlocked()) {
-        blockCount++
-        await page?.waitForTimeout(2000)
+        blockCount++;
+        await page?.waitForTimeout(2000);
         if (blockCount === 2) {
-          await browser?.close()
-          browser = null
-          page = null
-          blockCount = 0
+          await browser?.close();
+          browser = null;
+          page = null;
+          blockCount = 0;
         }
-        await page?.reload()
-        await page?.waitForTimeout(2000)
-        continue
+        await page?.reload();
+        await page?.waitForTimeout(2000);
+        continue;
       } else {
-        blockCount = 0
-        console.log('not blocked', blockCount)
-        const cookie = await getCookie(page)
+        blockCount = 0;
+        console.log("not blocked", blockCount);
+        const cookie = await getCookie(page);
 
         if (cookie === false) {
-          await page?.reload()
-          continue
+          await page?.reload();
+          continue;
         }
 
-        console.log(cookie)
-        saveCookie(cookie)
-        // if (!!cookie) {
-        //   break
-        // }
-        await page?.waitForTimeout(1 * 30 * 1000)
-        await page?.reload()
+        console.log(cookie);
+        saveCookie(cookie);
+        if (!!cookie) {
+          break;
+        }
+        await page?.waitForTimeout(1 * 30 * 1000);
+        await page?.reload();
       }
     } catch (e) {
-      console.log('Error! ->', e)
-      await browser?.close()
-      browser = null
+      console.log("Error! ->", e);
+      await browser?.close();
+      browser = null;
     }
   }
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Crunchbase/)
-})
+  await expect(page).toHaveTitle(/Crunchbase/);
+});
 
 const getCookie = async (page: any) => {
   return await page.evaluate(() => {
     return new Promise((resolve) => {
-      setTimeout(() => resolve(false), 1000 * 20)
-      ;(window as any).cookieStore?.addEventListener(
-        'change',
+      setTimeout(() => resolve(false), 1000 * 20);
+      (window as any).cookieStore?.addEventListener(
+        "change",
         async (e: any) => {
-          const cookie = e.changed[0]
-          if (cookie?.name === '_px3') {
+          const cookie = e.changed[0];
+          if (cookie?.name === "_px3") {
             resolve({
               token: document.cookie,
               agent: navigator.userAgent,
               expires: new Date(cookie.expires).toISOString(),
-            })
+            });
           }
-        },
-      )
-    })
-  })
-}
+        }
+      );
+    });
+  });
+};
 
 export const setDO = async (key: string, value: any) => {
   const res = await fetch(
     `https://api.rayst.workers.dev/do/${encodeURIComponent(key)}`,
     {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(value),
-    },
-  )
+    }
+  );
 
   if (res.status !== 200) {
-    throw new Error(`Failed to set DO: ${res.status}`)
+    throw new Error(`Failed to set DO: ${res.status}`);
   }
 
-  return res.json()
-}
+  return res.json();
+};
 
 export async function getDO<T = any>(key: string) {
   const res = await fetch(
-    `https://api.rayst.workers.dev/do/${encodeURIComponent(key)}`,
-  )
+    `https://api.rayst.workers.dev/do/${encodeURIComponent(key)}`
+  );
 
   if (res.status > 399) {
-    return null
+    return null;
   }
 
-  return res.json() as Promise<T>
+  return res.json() as Promise<T>;
 }
 
-const getData = () => getDO('ec321f3e-d0ec-496f-abc9-4a214ce6e5fc')
+const getData = () => getDO("ec321f3e-d0ec-496f-abc9-4a214ce6e5fc");
 
 const setData = (data: any) =>
-  setDO('ec321f3e-d0ec-496f-abc9-4a214ce6e5fc', data)
+  setDO("ec321f3e-d0ec-496f-abc9-4a214ce6e5fc", data);
 
 const saveCookie = async (newData: any) => {
   try {
-    const data: object[] = await getData()
+    const data: object[] = await getData();
 
-    await setData([newData, ...data])
+    await setData([newData, ...data]);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 
-  return newData
-}
+  return newData;
+};
